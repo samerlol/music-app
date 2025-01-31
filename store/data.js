803b567d1e064a421e7a493d431647b201d1980c
Samer Al3oory <samer.yousef@opensooq.com>
2025-01-31 11:42:11 +0300

    new push

--- store/data.js
+++ store/data.js
@@ -1,7 +1,7 @@
 // A collection of musicians where the key is the unique id
 module.exports = {
   coltrane: {
-    firstName: 'John',
+    firstName: 'John 2',
     lastName: 'Coltrane',
     genre: 'JAZZ',
   },
